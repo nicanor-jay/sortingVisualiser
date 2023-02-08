d9a49ac5db6736fc4ca6acd8ea1262abdd2503b2
njengu <nenguerra@gmail.com>
2023-02-08 21:36:00 +0000

    Added randomise button. TODO: Add button visualisation

--- Source.cpp
+++ Source.cpp
@@ -4,16 +4,18 @@
 #include <iostream>
 #include <stdlib.h> 
 
-
-
 const int screenWidth = 1000;
 const int screenHeight = 500;
 const int barAreaHeight = 400;
 
-const int numBars = 100;
+const int numBars = 200;
 const int barWidth = screenWidth / numBars;
 
-void populateBarArray(int barArray[]) {
+sf::RectangleShape rect;
+
+int barArray[numBars] = {};
+
+void populateBarArray() {
     // Populate array list for bar heights
     for (int i = 0; i < numBars; i++) {
         barArray[i] = (rand() % barAreaHeight) + 1;
@@ -21,17 +23,58 @@ void populateBarArray(int barArray[]) {
     }
 }
 
-void drawBarArray(sf::RenderWindow &window, int barArray[]) {
+void displayBarArray(sf::RenderWindow &window) {
     window.clear(sf::Color::Black);
     for (int k = 0; k < numBars; k++)
     {
-        sf::RectangleShape rect(sf::Vector2f(barWidth, barArray[k]));
+        rect.setSize(sf::Vector2f(barWidth, barArray[k]));
         rect.setFillColor(sf::Color::White);
         rect.setPosition(k * barWidth, barAreaHeight - barArray[k]);
         window.draw(rect);
     }
+
     window.display();
 }
+
+void bubbleSort(sf::RenderWindow& window) {
+    bool sorted = false;
+    bool swap = false;
+    int temp;
+
+    // Bubblesort Code
+    if (sorted ==false) {
+        for (int i = 0; i < (numBars - 1); i++)
+        {
+            swap = false;
+
+            for (int j = 0; j < numBars - i - 1; j++)
+            {
+                if ((barArray[j] > barArray[j + 1] )&& (sorted == false))
+                {
+                    std::cout << "Swapping index " << j << " & " << j + 1 << '\n';
+                    //Swap
+                    temp = barArray[j];
+                    barArray[j] = barArray[j + 1];
+                    barArray[j + 1] = temp;
+
+                    displayBarArray(window);
+
+                    swap = true;
+                }
+            }
+
+            if (swap == false)
+            {
+                sorted = truegit;
+                    
+            }
+        }
+    }
+    else {
+        std::cout << "Sorted\n";
+    }
+}
+
 int main()
 {
     // create the window
@@ -39,13 +82,14 @@ int main()
 
     int temp;
     bool swap = false;
-    bool sorted = false;
+    //bool sorted = false;
     sf::Vector2i mousePos;
-    sf::IntRect r1(0, barAreaHeight, 100, 100);
+    sf::IntRect buttonBubbleSort(0, barAreaHeight, 100, 100);
+    sf::IntRect buttonRandomise(100, barAreaHeight, 100, 100);
 
-    int barArray[numBars] = {};
+    sf::RectangleShape button(sf::Vector2f(100, 100));
 
-    populateBarArray(barArray);
+    populateBarArray();
 
     // run the program as long as the window is open
     while (window.isOpen())
@@ -54,61 +98,38 @@ int main()
         sf::Event event;
         while (window.pollEvent(event))
         {
+            switch (event.type)
+            {
             // "close requested" event: we close the window
-            if (event.type == sf::Event::Closed)
-                window.close();
+                case sf::Event::Closed:
+                    window.close();
+                case sf::Event::MouseButtonPressed:
+                    mousePos = sf::Mouse::getPosition(window);
+                    std::cout << mousePos.x << " " << mousePos.y << '\n';
+                   
+                    //Randomise bars
+                    if (buttonRandomise.contains(mousePos)) 
+                    {
+                        populateBarArray();
+                    }
 
-            if (event.type == sf::Event::MouseButtonPressed) {
-                mousePos = sf::Mouse::getPosition(window);
-                std::cout << mousePos.x << " " << mousePos.y << '\n';
-                if (r1.contains(mousePos)) {
-                    std::cout << "BUTTON PRESSED!!!!!!!!\n";
-                }
+                    //BubbleSort
+                    if (buttonBubbleSort.contains(mousePos)) 
+                    {
+                        std::cout << "BUTTON PRESSED!!!!!!!!\n";
+                        bubbleSort(window);
+                    }
             }
-
         }
 
         // clear the window with black color
-        //window.clear(sf::Color::Black);
+        window.clear(sf::Color::Black);
 
-        drawBarArray(window, barArray);
+        //button.setPosition(0, barAreaHeight);
+        //button.setFillColor(sf::Color::Green);
+        //window.draw(button);
 
-        // Bubblesort Code
-        if (sorted ==false) {
-            for (int i = 0; i < (numBars - 1); i++)
-            {
-                swap = false;
-
-                for (int j = 0; j < numBars - i - 1; j++)
-                {
-                    if ((barArray[j] > barArray[j + 1] )&& (sorted == false))
-                    {
-                        std::cout << "Swapping index " << j << " & " << j + 1 << '\n';
-                        //Swap
-                        temp = barArray[j];
-                        barArray[j] = barArray[j + 1];
-                        barArray[j + 1] = temp;
-
-                        swap = true;
-
-                        // Draw bars
-                        drawBarArray(window, barArray);
-                    }
-                }
-
-                if (swap == false)
-                {
-                    sorted = true;
-                    
-                }
-            }
-        }
-        else {
-            drawBarArray(window, barArray);
-            std::cout << "Sorted\n";
-        }
-        
-        //window.display();
+        displayBarArray(window);
     }
     return 0;
 }
